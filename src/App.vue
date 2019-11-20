3b96399258f3fa52599448725292a32b0dde5ad3
ankitsinghaniyaz <ankit.singhaniyaz@gmail.com>
2019-11-20 21:33:49 +0530

    add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header></header>
+    <aside></aside>
+    <main>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laborum impedit omnis placeat culpa corporis. Eius rem ipsa ipsum deleniti optio fugiat iure voluptatem earum adipisci eligendi atque, neque animi.</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laborum impedit omnis placeat culpa corporis. Eius rem ipsa ipsum deleniti optio fugiat iure voluptatem earum adipisci eligendi atque, neque animi.</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laborum impedit omnis placeat culpa corporis. Eius rem ipsa ipsum deleniti optio fugiat iure voluptatem earum adipisci eligendi atque, neque animi.</div>
+    </main>
+    <footer></footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.app {
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: repeat(3, auto);
+  grid-gap: 1em;
+  grid-template-areas: "header header"
+                       "facets main"
+                       "footer footer";
+}
+
+header {
+  min-height: 50px;
+  grid-area: header;
+  background: #ccc;
+}
+
+aside {
+  grid-area: facets;
+  background: tan;
+}
+
+main {
+  grid-area: main;
+  background: white;
+}
+
+footer {
+  min-height: 50px;
+  grid-area: footer;
+  background: #ccc;
 }
 </style>
